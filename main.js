9de02378ef0ecf5d2615b3a4ac15e261a6676f5d
DodySng <dodysinaga52.com>
2025-06-04 12:22:52 +0700

    ngetes aja wakkk

---
+++ main.js
@@ -0,0 +1,15 @@
+/*let nama = 'dody';
+let kelas = '23T02';
+let umur = 20;
+
+console.log(nama + ' ' + kelas + ' ' + umur)*/
+
+let cuaca = 'hujan';
+let keadaanTubuh = 'baik';
+let joging = true;
+
+if ((cuaca === 'panas' || cuaca === 'mendung') && (keadaanTubuh === 'baik' && joging)) {
+    console.log('gasskan joging boss   qqqq')
+}else{
+    console.log('dirumah aja dluu nanti sakitt kena hujan');
+}
