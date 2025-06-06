183bcd6337b0b38a93e3767ab52063a2548e90d6
DodySng <dodysinaga52.com>
2025-06-06 22:18:29 +0700

    BELAJAR TAMBAHAN

---
+++ belajarLopping.js
@@ -0,0 +1,36 @@
+//for loop
+/*for (let i = 0; i < 4; i++) {
+    console.log(i);
+}*/
+
+//while loop
+/*let cuaca = "cerah";
+
+while (cuaca !== "cerah"){
+    cuaca = prompt("cuaca hari ini:");
+}*/
+
+/*let angka;
+do {
+    angka = prompt("Masukkan angka > 10:"); 
+} while (angka <= 10);*/
+
+//For Of Loop
+/*let alamatRumah =["jln.gereja","jln.toba","jln.SKY"]
+for (let tempatTinggal of alamatRumah) {
+    console.log(tempatTinggal);
+}*/
+
+//for in loop
+/*const motor = {merek: "vario", tahun: 2018};
+for (const key in motor){
+    console.log(key,":", motor[key]);
+}*/
+
+//kontrol loop
+/*for(let i = 1; i <= 8; i++){
+    if(i === 8) break;
+    console.log(i);
+    
+}*/
+
